2188940b98df608d746d0892a1a8e759b3ebe704
Murat Fatih ARKAN <murat.fatih.arkan@gmail.com>
2018-05-29 23:21:58 +0300

    type json , xml formatı eklendi.

--- WebApiBasics/Global.asax.cs
+++ WebApiBasics/Global.asax.cs
@@ -3,6 +3,8 @@ using Castle.Windsor.Installer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Dispatcher;
@@ -16,6 +18,13 @@ namespace WebApiBasics
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
+
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("type", "xml", new MediaTypeHeaderValue("application/xml")));
+
             IWindsorContainer container = new WindsorContainer();
 
             container.Install(FromAssembly.This());
